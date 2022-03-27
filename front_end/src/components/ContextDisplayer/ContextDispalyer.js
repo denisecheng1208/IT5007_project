2e6f7b8730ea76eb5050c088c9bd1f1f95314f83
zs357 <zhengshi1998@foxmail.com>
2022-03-27 17:22:42 +0800

    Inplementation of adding comments components and display comments btn

--- front_end/src/components/ContextDisplayer/ContextDispalyer.js
+++ front_end/src/components/ContextDisplayer/ContextDispalyer.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
 import MDEditor from '@uiw/react-md-editor'
-
+import { ChatLeftText, PlusSquare } from 'react-bootstrap-icons'
 import './ContextDisplayer.css'
+import Textarea from '@uiw/react-md-editor/lib/components/TextArea/Textarea'
 
 export default class ContextDispalyer extends Component {
     state = {
         userId: "",
+        displayAddCommentTab: true,
+        addCommentTabOnDisplay: 0,
     }
 
     onContextClick = (idx) => {
-        this.props.changeDisplayState({commentsOnDisplay: idx})
+        if(idx == this.props.commentsOnDisplay){
+            this.props.changeDisplayState({ displayComments: !this.props.displayComments })
+        }else{
+            this.props.changeDisplayState({ commentsOnDisplay: idx, displayComments: true})
+        }
+        
+    }
+
+    addComments = (idx) => {
+        var cur = this.state.displayAddCommentTab
+        this.setState({ displayAddCommentTab: !cur, addCommentTabOnDisplay: idx })
     }
 
     render() {
@@ -19,14 +32,38 @@ export default class ContextDispalyer extends Component {
                     {
                         this.props.context.map((val, idx) => {
                             return (
-                                <div className="markDownContextWrapper" key={idx} onClick={() => this.onContextClick(idx)}>
-                                    <MDEditor.Markdown height={400} className="markDownContext" source={this.props.context[idx]}/>
+                                <div className="markDownContextWrapper row" key={idx} >
+                                    <MDEditor.Markdown height={400} className="markDownContext col-11" source={this.props.context[idx]} />
+                                    <div className="ctrlBtn col-1 row">
+                                        <div className="btnWrapper">
+                                            <button className='showComment btn col-12' onClick={() => this.onContextClick(idx)}>
+                                                <ChatLeftText />
+                                            </button>
+                                        </div>
+
+                                        <div className="btnWrapper">
+                                            <button className='addComment btn col-12' onClick={() => this.addComments(idx)}>
+                                                <PlusSquare />
+                                            </button>
+                                        </div>
+                                    </div>
+                                    {
+                                        this.state.displayAddCommentTab && this.state.addCommentTabOnDisplay == idx? 
+                                           (<div className='addCommentTab row'>
+                                                <hr className = "offset-5 col-2"/>
+                                                <textarea placeholder="Input your comments!" rows="5" type="text" className=' col-10'></textarea>
+                                                <div className="col-2">
+                                                    <button className="btn submitCommentBtn">Submit</button>
+                                                </div>
+                                                
+                                            </div>
+                                        ) : null
+                                    }
+                                    
                                 </div>
                             )
                         })
                     }
-                    
-                    
                 </div>
             </div>
         )
