5eb9629cc9b49e6e05eaa9c002cd5a37a6d3fd41
denisecheng1208 <1375606109@qq.com>
2022-03-31 11:08:33 +0800

    update WelcomePage
    Signed-off-by: denisecheng1208 <1375606109@qq.com>

--- front_end/src/components/WelcomePage/WelcomePage.js
+++ front_end/src/components/WelcomePage/WelcomePage.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react'
+import './WelcomePage.css'
+import { ListStars, FileEarmarkText } from 'react-bootstrap-icons'
+
 
 const initialDocs1 = [
     {
-      id: 1, owner: 'Ravan', created: new Date('2018-08-15'), 
-      title: 'HTML tutorial', type: 1,
+       id: 1, owner: 'Ravan', created: new Date('2018-08-15'), 
+       title: 'HTML tutorial', type: 1,
     },
     {
         id: 2, owner: 'Emma', created: new Date('2018-08-16'), 
         title: 'CSS tutorial', type: 1,
     },
+    {
+        id: 3, owner: 'Ella', created: new Date('2018-08-17'), 
+        title: 'React tutorial', type: 1,
+    },
+    {
+        id: 4, owner: 'Bob', created: new Date('2018-08-18'), 
+        title: 'Angular tutorial', type: 1,
+    },
+    {
+        id: 5, owner: 'Lily', created: new Date('2018-08-19'), 
+        title: 'Vue tutorial', type: 1,
+    },
 ];
 
 const initialDocs2 = [
     {
-      id: 3, owner: 'Bob', created: new Date('2018-08-17'), 
-      title: 'JAVA tutorial', type: 2,
+        id: 6, owner: 'Bob', created: new Date('2018-08-17'), 
+        title: 'JAVA tutorial', type: 2,
     },
     {
-        id: 4, owner: 'Mona', created: new Date('2018-08-18'), 
+        id: 7, owner: 'Mona', created: new Date('2018-08-18'), 
         title: 'MongoDB tutorial', type: 2,
+    },
+    {
+        id: 8, owner: 'ChengXin', created: new Date('2018-08-19'), 
+        title: 'Python tutorial', type: 2,
+    },
+    {
+        id: 9, owner: 'ShiZheng', created: new Date('2018-08-20'), 
+        title: 'C++ tutorial', type: 2,
+    },
+    {
+        id: 10, owner: 'PonyMa', created: new Date('2018-08-21'), 
+        title: 'Golong tutorial', type: 2,
     },
 ];
 
@@ -46,21 +73,60 @@ export default class WelcomePage extends Component {
         );
 
         return (
-            <div className="offset-1 col-10">
-                <div className='col-5'>
-                    <label>Front End</label>
-                    <br/>
-                    <ul>
-                        {docs1List}
-                    </ul>
-                </div>
-                <br/>
-                <div className='col-5'>
-                    <label>Back End</label>
-                    <ul>
-                        {docs2List}
-                    </ul>
-                </div>
+            <div className="row wrapper">
+                <div className='row offset-3 col-6 welcome'>Welcome to TechForum !</div>
+
+                <div className='row offset-3 col-6 blocks'>
+                    <div className='col-12 colorBar'>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<ListStars/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Front End
+                    </div>
+
+                    <ul className="col-10 offset-1 blockList">
+                    {
+                        this.state.docs1.map( d => {
+                            return (
+                            <li key={d.id} className="block row">
+                                <FileEarmarkText className='col-1'/>
+                                <div className="col-7">
+                                    {d.title}
+                                </div>
+                                <div className="col-2">
+                                    {d.created.toLocaleDateString()}
+                                </div>
+                                <div className="col-2">
+                                    {d.owner}
+                                </div>
+                            </li>)
+                        })
+                    }
+                     </ul>
+                </div> 
+
+                <div className='row offset-3 col-6 blocks'>
+                    <div className='col-12 colorBar'>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<ListStars/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Back End
+                    </div>
+
+                    <ul className="col-10 offset-1 blockList">
+                    {
+                        this.state.docs2.map( d => {
+                            return (
+                            <li key={d.id} className="block row">
+                                <FileEarmarkText className='col-1'/>
+                                <div className="col-6">
+                                    {d.title}
+                                </div>
+                                <div className="col-2">
+                                    {d.created.toLocaleDateString()}
+                                </div>
+                                <div className="col-2">
+                                    {d.owner}
+                                </div>
+                            </li>)
+                        })
+                    }
+                     </ul>
+                </div> 
             </div>
         );
     }
